712b06ffc067db7de058badbeb1fa0a85db2b650
xiayilaaiheti <shehla.speci@gmail.com>
2022-02-15 21:59:03 -0500

    Spring-01-Core-IoC added

---
+++ Spring-01-Core-IoC/src/main/java/com/cydeo/bean_annotation/CydeoApp.java
@@ -0,0 +1,15 @@
+package com.cydeo.bean_annotation;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class CydeoApp {
+    public static void main(String[] args) {
+
+        ApplicationContext container = new AnnotationConfigApplicationContext(ConfigApp.class,ConfigAny.class);
+
+        String str = container.getBean(String.class);
+        System.out.println(str);
+
+    }
+}
