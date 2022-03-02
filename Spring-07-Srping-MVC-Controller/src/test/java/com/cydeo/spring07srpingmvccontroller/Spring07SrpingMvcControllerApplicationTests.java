8652798485a7e2d02ff98d5367650a41a1ddfb78
xiayilaaiheti <shehla.speci@gmail.com>
2022-03-01 21:37:42 -0500

    Spring-07 MVC-Controller

---
+++ Spring-07-Srping-MVC-Controller/src/test/java/com/cydeo/spring07srpingmvccontroller/Spring07SrpingMvcControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.spring07srpingmvccontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring07SrpingMvcControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ demo/Spring-08-MVC-Spring-Model/src/main/java/com/example/spring08mvcmodel/Spring08MvcModelApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring08mvcmodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring08MvcModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring08MvcModelApplication.class, args);
+	}
+
+}
