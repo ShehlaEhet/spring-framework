34571533efa9a653208fdfcfb87c665a8566e204
xiayilaaiheti <shehla.speci@gmail.com>
2022-04-28 19:59:21 -0400

    ORM - Derived, JPQL, Native

--- Spring-14-ORM-Queries/src/main/java/com/cydeo/bootstrap/DataGenerator.java
+++ Spring-14-ORM-Queries/src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -1,10 +1,14 @@
 package com.cydeo.bootstrap;
 
+import com.cydeo.entity.Course;
+import com.cydeo.repository.CourseRepository;
 import com.cydeo.repository.DepartmentRepository;
 import com.cydeo.repository.EmployeeRepository;
 import com.cydeo.repository.RegionRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class DataGenerator implements CommandLineRunner {
@@ -12,11 +16,13 @@ public class DataGenerator implements CommandLineRunner {
     private final RegionRepository regionRepository;
     private final DepartmentRepository departmentRepository;
     private final EmployeeRepository employeeRepository;
+    private final CourseRepository courseRepository;
 
-    public DataGenerator(RegionRepository regionRepository, DepartmentRepository departmentRepository, EmployeeRepository employeeRepository) {
+    public DataGenerator(RegionRepository regionRepository, DepartmentRepository departmentRepository, EmployeeRepository employeeRepository, CourseRepository courseRepository) {
         this.regionRepository = regionRepository;
         this.departmentRepository = departmentRepository;
         this.employeeRepository = employeeRepository;
+        this.courseRepository = courseRepository;
     }
 
     @Override
@@ -50,6 +56,34 @@ public class DataGenerator implements CommandLineRunner {
         System.out.println("getEmployeeSalary:" + employeeRepository.getEmployeeSalary());
 
         System.out.println("-----------------EMPLOYEE END-----------------");
+
+
+        System.out.println("-----------------COURSE START-----------------");
+
+
+        //implement the method that i created in repository, meaning inject the method here
+        courseRepository.findByCategory("Spring").forEach(System.out::println);
+
+
+        courseRepository.findByCategoryOrderByName("Spring").forEach(System.out::println);
+
+
+
+        System.out.println(courseRepository.existsByName("JavaScript for All"));
+
+
+        System.out.println(courseRepository.countByCategory("Spring"));
+
+
+        courseRepository.findByNameStartsWith("Scalable").forEach(System.out::println);
+
+        System.out.println("-------------------------------------");
+
+        courseRepository.streamByCategory("Spring").forEach(System.out::println);
+
+
+        System.out.println("-----------------COURSE END-----------------");
+
 
 
     }
--- Spring-14-ORM-Queries/src/main/java/com/cydeo/repository/EmployeeRepository.java
+++ Spring-14-ORM-Queries/src/main/java/com/cydeo/repository/EmployeeRepository.java
@@ -2,8 +2,11 @@ package com.cydeo.repository;
 
 import com.cydeo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
@@ -51,6 +54,66 @@ public interface EmployeeRepository extends JpaRepository<Employee, String> {
 
     @Query("SELECT e from Employee e where e.email=?1 and e.salary=?2")
     Employee getEmployeeDetail(String email, int salary);
+
+    //Not Equal
+    @Query("SELECT e FROM Employee e WHERE e.salary <> ?1")
+    List<Employee> getEmployeeSalaryNotEqual(int salary);
+
+    //like/contains/startswith/endswith
+    @Query("SELECT e FROM Employee e WHERE e.firstName LIKE ?1")
+    List<Employee> getEmployeeFirstNameLike(String pattern);
+
+    //less than
+    @Query("SELECT e FROM Employee e WHERE e.salary < ?1")
+    List<Employee> getEmployeeSalaryLessThan(int salary);
+
+    //greater than
+    @Query("SELECT e FROM Employee e WHERE e.salary > ?1")
+    List<Employee> getEmployeeSalaryGreaterThan(int salary);
+
+    //Before
+    @Query("SELECT e FROM Employee e WHERE e.hireDate > ?1")
+    List<Employee> getEmployeeHireDateBefore(LocalDate date);
+
+    //Between
+    @Query("SELECT e FROM Employee e WHERE e.salary between ?1 and ?2")
+    List<Employee> getEmployeeSalaryBetween(int salary1, int salary2);
+
+    //Null
+    @Query("SELECT e FROM Employee e WHERE e.email IS NULL")
+    List<Employee> getEmployeeEmailIsNull();
+
+    //NotNull
+    @Query("SELECT e FROM Employee e WHERE e.email IS NOT NULL")
+    List<Employee> getEmployeeEmailIsNotNull();
+
+    //Sorting in ascending order
+    @Query("SELECT e FROM Employee e ORDER BY e.salary")
+    List<Employee> getEmployeeSalaryOrderAsc();
+
+    //Sorting in descending order
+    @Query("SELECT e FROM Employee e ORDER BY e.salary DESC")
+    List<Employee> getEmployeeSalaryOrderDesc();
+
+    @Query(value = "SELECT * FROM employees WHERE salary ?1", nativeQuery = true)
+    List<Employee> readEmployeeDetailBySalary(int salary);
+
+    @Query("SELECT e FROM Employee e WHERE e.salary = :salary")
+    List<Employee> getEmployeeSalary(@Param("salary") int salary);
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE Employee SET email = 'admin@email.com' WHERE id=:id", nativeQuery = true)
+    void updateEmployeeJPQLNativeQuery(@Param("id")int id);
+
+
+
+
+
+
+
+
+
 
 
 
